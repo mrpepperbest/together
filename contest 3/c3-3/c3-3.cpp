30fa91ca37195d5a2614b05a6456131f24fa930a
Mark <bodrovmark@mail.ru>
2020-02-19 15:24:08 +0300

    c3-problem 3

---
+++ contest 3/c3-3/c3-3.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+	// Погладить данную зверушку.
+	// Последствия зависят от реализации данного метода для класса конкретной зверушки.
+	virtual void pet() = 0;
+
+	virtual ~Animal() {};
+};
+class NPC {
+public:
+	// Поговорить с NPC.
+	// Что он скажет - зависит от реализации данного метода для конкретного NPC.
+	virtual void talk() = 0;
+
+	virtual ~NPC() {};
+};
+class SmartCat : public Animal, public NPC {
+public:
+	SmartCat() {};
+	~SmartCat() {};
+	void talk() override {
+		cout << "Meow!" << endl;
+	};
+	void pet() override {
+		cout << "Meow!" << endl;
+	};
+};
+int main(void) {
+	// Примерно так с нашим классом будут обращаться части движка, 
+// которые вообще-то отвечают за взаимодействие со зверями.
+	Animal* a = new SmartCat();
+	a->pet();
+	delete a;
+
+	// А так - части движка, которые работают с NPC.
+	NPC* n = new SmartCat();
+	n->talk();
+	delete n;
+	return 0;
+}
