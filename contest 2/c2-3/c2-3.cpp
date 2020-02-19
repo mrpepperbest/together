840495d8ecd653182322c7516ded3c7d8780e7e2
Mark <bodrovmark@mail.ru>
2020-02-19 14:03:02 +0300

    problem 3

---
+++ contest 2/c2-3/c2-3.cpp
@@ -0,0 +1,32 @@
+﻿class Material
+{
+public:
+	// Принимает на вход величину деформации.
+	// Возвращает величину напряжения, посчитанную с учётом реологии материала.
+	virtual float getStress(float strain) = 0;
+};
+class ElasticMaterial : public Material {
+public:
+	float emod;
+	ElasticMaterial(float elasticModulus){
+		this->emod = elasticModulus;
+	};
+	float getStress(float strain) {
+		return (this->emod * strain);
+	};
+};
+class PlasticMaterial : public Material {
+public:
+	float emod;
+	float sLim;
+	PlasticMaterial(float elasticModulus, float strainLimit) {
+		this->sLim = strainLimit;
+		this->emod = elasticModulus;
+	};
+	float getStress(float strain) {
+		if (strain >= sLim) {
+			return this->sLim * this->emod;
+		}
+		return strain * this->emod;
+	};
+};
