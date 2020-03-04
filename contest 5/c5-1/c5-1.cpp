bb39b5477f07f07639e33cd4044321ae15d50d25
Mark <bodrovmark@mail.ru>
2020-03-04 13:27:34 +0300

    a

---
+++ contest 5/c5-1/c5-1.cpp
@@ -0,0 +1,54 @@
+﻿
+#include <iostream>
+#include <string>
+
+class Student
+{
+private:
+	std::string name;
+	unsigned int score;
+public:
+	Student() {
+		name = '\0';
+		score = 0;
+	};
+	~Student() {};
+	// Задать имя студента
+	void setName(std::string name) {
+		this->name = name;
+	};
+
+	// Указать количество баллов за контрольную
+	void setScore(unsigned int score) {
+		this->score = score;
+	};
+
+	// Получить имя студента
+	std::string getName() const {
+		return name;
+	};
+
+	// Получить количество баллов студента
+	unsigned int getScore() const {
+		return score;
+	};
+};
+std::istream& operator>>(std::istream& is, Student& a) {
+	std::string b;
+	getline(is,b);
+	a.setName(b);
+	a.setScore(0);
+	return is;
+}
+std::ostream& operator<<(std::ostream& os, const Student& a) {
+	os << "'" << a.getName() <<"': " << a.getScore() << std::endl;
+	return os;
+}
+int main()
+{
+	Student s;
+	std::cin >> s;
+	s.setScore(10);
+	std::cout << s << std::endl;
+	return 0;
+}
---
+++ с1/с1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+class SpacePort
+{
+public:
+	int* docks;
+	int max_dock_num;
+	// Создать космодром, в котором size штук доков.
+	// Доки нумеруются от 0 до size-1.
+	// В момент создания все доки свободны.
+	SpacePort(unsigned int size) {
+		docks = new int[size];
+		for (int i = 0; i < size; ++i) {
+			docks[i] = 0;
+		}
+		this->max_dock_num = size-1;
+	};
+	~SpacePort() {
+		delete[] docks;
+	};
+
+	// Запросить посадку в док с номером dockNumber.
+	// Если такого дока нет - вернуть false (запрет посадки).
+	// Если док есть, но занят - вернуть false (запрет посадки).
+	// Если док есть и свободен - вернуть true (разрешение посадки) и док становится занят.
+	bool requestLanding(unsigned int dockNumber) {
+		if (dockNumber > this->max_dock_num) {
+			return false;
+		}else
+		if (this->docks[dockNumber]==1) {
+			return false;
+		}
+		docks[dockNumber] = 1;
+		return true;
+	};
+
+	// Запросить взлёт из дока с номером dockNumber.
+	// Если такого дока нет - вернуть false (запрет взлёта).
+	// Если док есть, но там пусто - вернуть false (запрет взлёта).
+	// Если док есть и в нём кто-то стоит - вернуть true (разрешение взлёта) и док становится свободен.
+	bool requestTakeoff(unsigned int dockNumber) {
+		if (dockNumber > this->max_dock_num) {
+			return false;
+		}else
+		if (this->docks[dockNumber] == 0) {
+			return false;
+		}
+		this->docks[dockNumber] = 0;
+		return true;
+	};
+};
+int main(void) {
+	SpacePort s(5);
+	cout << boolalpha << s.requestLanding(0) << endl;
+	cout << boolalpha << s.requestLanding(4) << endl;
+	cout << boolalpha << s.requestLanding(5) << endl;
+
+	cout << boolalpha << s.requestTakeoff(0) << endl;
+	cout << boolalpha << s.requestTakeoff(4) << endl;
+	cout << boolalpha << s.requestTakeoff(5) << endl;
+	return 0;
+}
